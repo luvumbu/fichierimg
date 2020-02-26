e1e3694fad89d2bbb49326e8cdd77ebae62e310b
unknown <luvumbu.n@gmail.com>
2020-02-26 14:47:33 +0100

    ok

---
+++ index.php
@@ -0,0 +1,18 @@
+<form>
+    <input type="file" name="" id="file-input" /><br />
+    <input type="submit" value="Upload" id="submit-button" />
+</form>
+
+<div id="upload-progress"></div>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="upload.js"></script>
+
+
+<style>
+#upload-progress
+{
+    font-size:3em ; 
+    color:red;
+}
+</style>
